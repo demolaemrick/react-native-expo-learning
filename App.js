947f7a8757cc20ceb516196a71bdf146c1be358c
Ademola Akindotuni <emricky17@gmail.com>
2023-07-19 11:47:15 +0100

    Button component

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, View } from 'react-native';
+import Button from './components/Button';
+
 
 const PlaceholderImage = require('./assets/images/background-image.png');
-
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
       <StatusBar style="light" />
     </View>
@@ -30,5 +35,9 @@ const styles = StyleSheet.create({
     width: 320,
     height: 440,
     borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
